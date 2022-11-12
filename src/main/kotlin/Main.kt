4e757a7a80bbb9e5bab99f171a14a4dd679f647f
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-12 09:55:32 +0300

    Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>) {
 
     val name = "Hello Benjamin!"
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-//    println("Program arguments: ${args.joinToString()}")
+//     Try adding program arguments via Run/Debug configuration.
+//     Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+//     println("Program arguments: ${args.joinToString()}")
 
     hello(name)
     println(hello(name))
