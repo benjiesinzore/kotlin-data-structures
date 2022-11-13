2946bf4013f1de5ce3efac23fd93f32bcd1828f3
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-13 22:24:58 +0300

    Initial Commit - Introduction Class

--- src/main/kotlin/Introduction.kt
+++ src/main/kotlin/Introduction.kt
@@ -3,3 +3,9 @@
 *  How to create custom data-types
 *  Hw to manipulate data in loops and control structures
 */
+
+// Variable is a way to store data, they have names types and sometimes modifies
+// There are two types of variables in Kotlin val and var, val variables can not be reassigned
+
+val name = "Benjamin Sinzore"
+var age = 25
